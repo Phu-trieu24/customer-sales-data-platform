9c753320cfd584c0829835b257fd9c552263cae5
Phu-trieu24 <trieu.phu24@gmail.com>
2026-02-11 11:55:11 +0200

    ETL to PostgresDB

--- etl/run_pipeline.py
+++ etl/run_pipeline.py
@@ -1,10 +1,12 @@
 from src.ingest import ingest_data
 from src.transform import transform_data
-from src.load import load_to_csv
+# from src.load import load_to_csv
+from src.load import load_to_postgres
+
 
 RAW_PATH = "data/raw/online_retail.csv"
-PROCESSED_PATH = "data/processed/retail_transformed.csv"
-
+PROCESSED_PATH = "data/processed/online_retail_transformed.csv"
+CONN_STR = "postgresql://analyst:mypassword123@localhost:5432/sales_analytics"
 def run_pipeline():
     print("📥 Ingesting data...")
     df_raw = ingest_data(RAW_PATH)
@@ -13,7 +15,13 @@ def run_pipeline():
     df_transformed = transform_data(df_raw)
 
     print("📤 Loading data...")
-    load_to_csv(df_transformed, PROCESSED_PATH)
+    # load_to_csv(df_transformed, PROCESSED_PATH)
+
+    load_to_postgres(
+        df_transformed,
+        table_name="retail_sales",
+        conn_str=CONN_STR
+    )
 
     print("✅ Pipeline finished successfully")
 
--- src/load.py
+++ src/load.py
@@ -1,7 +1,31 @@
+# import pandas as pd
+
+# #load_to_CSV file local
+# def load_to_csv(df: pd.DataFrame, path: str):
+#     """
+#     Save transformed data to CSV
+#     """
+#     df.to_csv(path, index=False)
+
+# load_to_postgres
+from sqlalchemy import create_engine
 import pandas as pd
 
-def load_to_csv(df: pd.DataFrame, path: str):
+def load_to_postgres(
+    df: pd.DataFrame,
+    table_name: str,
+    conn_str: str,
+    if_exists: str = "append"
+):
     """
-    Save transformed data to CSV
+    Load DataFrame to Postgres table
     """
-    df.to_csv(path, index=False)
+    engine = create_engine(conn_str)
+
+    df.to_sql(
+        table_name,
+        engine,
+        if_exists=if_exists,
+        index=False,
+        method="multi"
+    )
